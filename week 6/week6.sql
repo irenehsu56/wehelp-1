d59c481ecc54866f3d82f130c42d407876ea6eb0
irenehsu56 <fhgjnmyswb@privaterelay.appleid.com>
2026-07-19 00:32:21 +0800

    Create week6.sql

---
+++ week 6/week6.sql
@@ -0,0 +1,23 @@
+create database if not exists website;
+use website;
+create table if not exists member(
+	id int unsigned primary key auto_increment,
+    name varchar(255) not null,
+    email varchar(255) not null,
+    password varchar(255) not null,
+    follower_count int unsigned not null default 0,
+    time datetime not null default current_timestamp
+);
+alter table member add constraint unique_member_email unique(email);
+select * from member;
+
+use website;
+create table if not exists message(
+	id int unsigned primary key auto_increment,
+    member_id int unsigned not null,
+    content text not null,
+    like_count int unsigned not null default 0,
+    time datetime not null default current_timestamp,
+    foreign key(member_id) references member(id)
+);
+select * from message;
